c79a0b30f5ab1649b1745300374d26d60f745bcb
joao <barrenha95@yahoo.com.br>
2024-01-22 08:21:09 -0300

    p1_transformations

--- project_1/p1_model.py
+++ project_1/p1_model.py
@@ -23,3 +23,24 @@ df = pd.DataFrame(dsa_data)
 # Split X (input) and Y (output)
 X = df[['Package_Weight', 'Package_Type']]
 y = df['Product_Type']
+
+# Split in train and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+# Creation and fit the transformations in the training data
+
+# Fit of the categorical feature "Package_Type"
+le_package_type = LabelEncoder() #Assing a number to each category
+le_package_type.fit(X_train['Package_Type']) # Using the train data, I train my "encoder"
+
+# Fit of the categorical feature "Product_Type"
+le_product_type = LabelEncoder()
+le_product_type.fit(y_train)
+
+# Apply the transformation in the train and test data
+X_train['Package_Type'] = le_package_type.transform(X_train['Package_Type'])
+X_train['Package_Type'] = le_package_type.transform(X_test['Package_Type'])
+
+y_train = le_product_type.transform(y_train)
+y_test = le_product_type.transform(y_test)
+
