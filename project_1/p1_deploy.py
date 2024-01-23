4650f6f004cb70c7c2722636fb2ff688f64b4509
joao <barrenha95@yahoo.com.br>
2024-01-23 08:33:14 -0300

    deploy-software

--- project_1/p1_deploy.py
+++ project_1/p1_deploy.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 # Load of the model and the transformations
 dsa_model = joblib.load('project1/models/logistic_model.pkl')
-transformation_package_type = joblib.load('project1/models/transformation_package_type.pkl')
-transformation_product_type = joblib.load('project1/models/transformation_product_type.pkl')
+le_package_type = joblib.load('project1/models/transformation_package_type.pkl')
+le_product_type = joblib.load('project1/models/transformation_product_type.pkl')
 
 # Assign the principal route for the homepage and allow only "GET" requisitions.
 @app.route('/', methods = ['GET'])
@@ -23,6 +23,22 @@ def index():
 def predict():
 
     # Extract the "weight" value from the sent requisition
-    peso = int(request.form['Weight'])
+    weight = int(request.form['Weight'])
 
-    
+    # Tranform the package_type using the label encoder previously fited
+    package_type = le_package_type.transform([request.form['package_type']])[0]
+
+    # Use the model to make the prediction
+    prediction = dsa_model.predict([[weight, package_type]])[0]
+
+    # Convert the encoded prediction to it original label
+    product_type = le_product_type.inverse_transform([prediction])[0]
+
+    # Render the homepage with the prediction
+    return render_template('template.html', prediction = product_type)
+
+# App
+## This is the key that says it is a software instead of a script
+### Or in other words: It MUST be runned as only one thing
+if __name__ == '__main__':
+    app.run()
