e052f4ebc5b8d96bbbf9c37753b01db1577a284f
Joao Victor Isa Barrenha <joao.barrenha@clear.sale>
2024-02-18 08:03:39 -0300

    first-model-logistic-regression

--- project_3/p3_model.py
+++ project_3/p3_model.py
@@ -37,3 +37,26 @@ print(X_train_scaled.shape)
 print(X_test_scaled.shape)
 
 ## First version of the model ##
+# For the first version we will use LogisticRegression because of it simplicity to explain the weights of each feature.
+# This model calculates the chance of the input be in each category giving a probability between 0 and 1.
+# The most recommended performance metrics for this model is: Accuracy, Confusion Matrix, and AUC-ROC.
+# It's usually the "benchmark" model because it is the more simple machine learning model.
+
+model_v1 = LogisticRegression()
+
+model_v1.fit(X_train_scaled, y_train)
+y_pred_v1 = model_v1.predict(X_test_scaled)
+print(y_pred_v1)
+
+y_pred_prob_v1 = model_v1.predict_proba(X_test_scaled)[:,1]
+print(y_pred_prob_v1)
+
+acc_v1 = accuracy_score(y_test, y_pred_v1)
+print(f"\nAccuracy: {acc_v1}")
+
+auc_v1 = roc_auc_score(y_test, y_pred_prob_v1)
+print(f"\nAuc: {auc_v1}")
+
+class_v1 = classification_report(y_test, y_pred_v1)
+print(f"\Classification report: {class_v1}")
+
